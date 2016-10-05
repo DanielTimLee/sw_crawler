93d036c758061033ea3d66fb34c004611b833965
Daniel Tim Lee <danieltimlee@gmail.com>
2016-10-05 21:46:19 +0900

    requirements.txt updated

---
+++ sw_crawler_categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO sw_crawler.categories (name, title, created_date) VALUES ('family', '가족 및 결혼/연애 상태', null);
+INSERT INTO sw_crawler.categories (name, title, created_date) VALUES ('business', '비지니스 및 산업', null);
+INSERT INTO sw_crawler.categories (name, title, created_date) VALUES ('shopping', '쇼핑 및 패션', null);
+INSERT INTO sw_crawler.categories (name, title, created_date) VALUES ('sports', '스포츠 및 야외활동', null);
+INSERT INTO sw_crawler.categories (name, title, created_date) VALUES ('food', '식품 및 음료', null);
+INSERT INTO sw_crawler.categories (name, title, created_date) VALUES ('entertain', '엔터테인먼트', null);
+INSERT INTO sw_crawler.categories (name, title, created_date) VALUES ('hobby', '취미 및 활동', null);
+INSERT INTO sw_crawler.categories (name, title, created_date) VALUES ('tech', '테크놀로지', null);
+INSERT INTO sw_crawler.categories (name, title, created_date) VALUES ('health', '피트니스 및 웰빙', null);
---
+++ sw_crawler_documents.sql
@@ -0,0 +1,95 @@
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '우리나라 청년들의 Big5 조사결과 내용 분석. 어쩌다 이런 조사자료까지 내가 보고있는지는 나도 모름 (..) 하지만 그나마 최근 조사자료이고, 통계적 의미가 있는 내용만을 본다고 가정할때, 스테레오 타입이 괜히 만들어지는건 아니라는 생각도 듬.
+아래는 보고서 내용 중 발췌:
+성별로는 정서불안과 성실성에서 통계적으로 유의미한 차이를 가지며, 개방성에서도 크진 않으나 통계적으로 유의미한 차이가 있음.
+–남성의 성격 요인 점수는 외향성 10.33점, 정서불안 7.28점, 친화성 10.81점, 성실성 10.05점, 개방성 9.87점임.
+–청년층 남성은 여성보다 정서불안이 1.36점 낮고, 성실성은 0.30점, 개방성은 0.21점 더 높은 것으로 나타남
+출신고교별 성격 특성은 외국어고의 경우 외향성 10.78점, 친화성 11.37점, 개방성 10.73점으로 다른 출신고교에 비해 높은 점수를 나타냈으며, 통계적으로도 유의미한 것으로 나타남.
+-과학고는 정서불안이 7.35점으로 다른 출신고교에 비해 가장 낮은 점수를 가지며, 통계적으로도 유의미한 것으로 나타남.
+-특성화고는 외향성 10.10점, 친화성 10.52점, 개방성 9.33점으로 다른 출신고교에 비해 가장 낮으며, 정서불안은 8.32점으로 가장 높게 나타남.
+학력상태별 BIG5 성격 특성을 보면 외향성은 고등학교 졸업자, 전문대 경험자(재학, 졸업 포함), 대학 경험자(재학, 졸업 포함) 순으로 학력과 비례하는 반면, 대학원 경험자(재학, 졸업 포함)는 상대적으로 외향성이 가장 낮게 나타남
+', 'family,hobby', '2016-09-28 11:09:51');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '혹시 제휴 마케팅에 관련해서 조언(전문가 인터뷰 형태)을 주실 수 있는 분을 소개해주실수 있을까요? 배우고 싶습니다. #마알못 #제알못
+', null, '2016-09-28 11:09:51');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '가정교육 방법에 대한 비평을 듣고 순간 욱했다. 아직도 내가 깨어져야 할 부분을 찾은것 같아 씁쓸했다.
+곱씹어볼수록 우리가정은 뭔가 논리와 계획에 의해 만들어져간다기보단 그저 은혜로 사는것 같다. 참 감사하다.
+', 'entertain', '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '작년의 감동을 다시 한번 #artree #gospel #musical
+', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '[그러나] 나는 내게 이로웠던 것은 무엇이든지 그리스도 때문에 해로운 것으로 여기게 되었습니다. (빌3:7, 새번역)
+I once thought these things were valuable, but now I consider them worthless because of what Christ has done. (Philippians 3:7, NLT)
+', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '으걐 ㅋㅋㅋㅋㅋ ㅋㅋㅋㅋㅋㅋ ㅋㅋㅋ
+', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '
+#거왠만하면눔코치하지', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '제주도가 전국 평균 체지방률이 높아서 팔리는건 그런가보다 했다. 그런데 도대체 기독교인이 눔다이어트 많이 사는 이유는 뭐냐.
+앞으로 인과관계 따지지 않고 나는 그냥 꾸준히 숫자마케팅 해야할듯. #주님이뱃살을회개합니다
+
+
+', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '인터콤 이자슥들. 돈 많이 벌어라.
+', 'family', '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '이번에 채용 컨설팅을 받으면서 또 다르게 시도했던점은 내가 사람보는 기준을 업그레이드해야한다는 점. 추천 받은 방식은 내 기준으로 한번 평가를 하고, 실제 퍼포먼스 기준으로 또 한번 평가를 해보는 것. 그리고 왜 차이가 있었는지를 분석하는 것.
+오늘 그 2차 결과를 확인했고 고로 나는 앞으로 마케터를 뽑는데 관여하지 않기로 마음 먹었다. 누가 누굴 평가해 ㅋㅋㅋㅋ
+June Kim 님 우짤까요. ㅠㅜ)', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '채용 과정에 있어서 우리나라 경력직 분들의 레퍼체크를 하는 노하우들이 있으실까요?
+저희는 그냥 대놓고 체크포인트를 요청하고 연락을 하는데 이 방식 보다 더 좋은 방법이 있을것 같아서요.', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '그 중 제일은 사랑이라.
+', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '경쟁이 아닌 인격. 자리가 아닌 사명.
+', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '#눔코치 와 매일매일 따라할수 있는 영상.
+집에서 켜두고 10분만 따라한뒤, 어플에 기록하면 코치분에게 명확한 피드백을 받을 수 있어요!', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '혹시 현대 남성 직장인이 여성 동료에 대한 올바른 인재상과, 협업 방법에 대해 배울 수 있는 서적이나 아티클을 추천해주실수 있을까요?
+요즘 여성 동료들을 채용하기 위해 평가해야하는 경우가 많은데, 스스로 무의식속에 녹아있던 잘못된 선입견이 있다는 사실들을 발견하고 깜짝깜짝 놀랍니다.
+우선 제대로 배워야 내가 올바르게 행동하고, 아들냄도 제대로 가르칠 수 있기에 고수분들의 가르침을 요청합니다.', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '눔코리아의 오랜 레거시 중 하나인 웹호스팅 서버를 AWS로 이전하면서 정말 밀린 빨래를 다 하는 것 같다 ㅠㅜ)
+Cafe24 다신 처다보지도 않을거임.', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '혹시 평소에 필사를 정기적으로 하시는 분이 계신가요?
+어떤 효과를 기대하고 하시나요? 그 효과를 언제 보셨다고 기억하시나요?', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '
+명절 선물의 딜레마.
+만족과 헤아림의 끝이란 없는 것 같다.
+진심만 통할 뿐.', 'family', '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '눔코치가 역사상 최초로 당뇨병을 예방할 수 있는 모바일 헬스 솔루션으로 입증 되었습니다.
+수년간의 임상 자료로 발표된 이번 연구 결과는 눔이 개발한 24주 당뇨병 예방 프로그램을 통해 스마트폰 앱만으로 64% 의 참여자들이 transformative weight loss (당뇨병에 의미있는 예방을 가져올 수 있는 5% 이상의 체중 감량 및 건강신호 개선)를 이뤄냈다고 입증해냈습니다.
+이게 어느 정도냐고요? 미국 CDC(질병관리본부)에서 발표한 자료에 대입하자면 눔 당뇨병 예방효과를 얻은 68%의 대상들이 당뇨병에 걸릴 확률을 58%나 줄였다고 합니다. 당뇨병에 걸리게되면 평균 수명이 10년 정도 줄어든다는 연구를 대입해본다면, 5.8년을 늘려줬다고 말할수 있어요!
+정말 많은 동료들이 단순 다이어트 뿐만 아니라, 만성질환을 달고 살아가는 인구들을 위해 건강한 습관 변화를 만들수 있도록 노력하고 있습니다. 그 노력을 인정 받았다고 하니 기쁘고 또 기대됩니다.
+자랑스럽습니다. 눔! 칭찬해주세요!', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '그레이트한 여정의 시작!
+#SMTM팀 #오픈서치프로젝트', 'food,entertain,hobby', '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '킨더 초콜릿 보물찾기 놀이.
+새벽에 들어오고, 자고있을때 나가는 업무 일정. 주말에 집에 있더라도 요즘 컨콜에 채용 면접에, 바뀐 보직때문에 스터디하랴 눈길조차 적게 주는 나쁜 아빠와 함께하는 본격 비동기적 놀이.
+아빠는 아이가 잠든 시간에 집안 곳곳에 아이 숫자에 맞춰 상품을 숨겨둔다. (킨더초콜릿에 만족하는 이유는, 아이들이 좋아하고 그 안에 가챠 효과가 한번 더 있기 때문) 애들이 아침에 눈을 뜨면 그 초콜릿을 찾아 헤메고 다닌다. 그리고 발견하면 셀카를 찍어 가족 클라우드 앨범에 올린다. 이렇게 찍기 시작한 사진들이 모이기 시작하니 루틴이 됨.
+추가로 발견된 유익점들:
+1) 아침에 잠투정을 하지 않음. (깨우려면 최소 5분은 밍기적 대던 아이들이, 얘들아 아빠가 숨긴 킨더 찾아야지~ 하면 벌떡 일어남)
+2) 비동기적이기 때문에 아빠가 유지보수하는데 큰 걱정이 없음. 심지어 실제로 내가 살필요도, 숨길 필요도 없음 (지금은 재미로 내가 함)
+3) 초콜릿을 아침으로 먹는건 현재까지 딱히 단점을 못찾았는데, 조만간 다른 베리에이션 테스트 해봐야할듯.
+아침에 출근했는데, 클라우드 폴더에 애들이 성공해서 찍은 사진들 올라오면 흐뭇함. 다둥이 아빠 입장에서는 행복도에 도움됨. 가끔 전화옴 왤케 난이도가 높냐고. ㅋㅋ 욘석들아 인생이 그런거야', 'health', '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '먹고 힘내라심
+', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '광고 채널에 겨우 100만원 써보고 어떻게 아냐고 한다.
+모르면 이상하지!
+', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', 'B2C 그로스 8개월째. 폴 그래이엄의 주간 누적 5% 성장을 목표로 꾸역꾸역 넘겨온지도 25주가 넘어간다.
+그로스를 배우면 배울수록 느끼는 것은, 책속의 대단한 사례들과 실제 그로스해커로써의 일상은 매우 다르다라는 것. 마치 척박한 개척지에서 농작을 시작한 농부의 마음과도 같다.
+하루하루 밭을 갈고, 씨앗을 뿌리고, 잡초를 뽑는 일은 물론. 정기적으로 높은 곳에 올라서서 밭의 형태와 미래의 모습을 점검하는 것은 물론. 함께 개척지를 일구고 땀을 흘릴 동료 농부들을 찾고, 교육시키고 동기부여하는 등.
+가끔씩 주변 사물이 희미해지고, 터널 비전 증후군에 빠져드는듯한 느낌이 들면. 업의 본질을 돌이켜본다. 그리곤 함께 곡괭이질하는 동료들의 땀과 성장을 본다.
+이게 농부의 마음. 밭을 가꾸는 보람.', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '복된 인생으로 살고 죽기 위해서 당신은 무엇을 알아야 합니까?
+다음의 세 부분을 알아야 합니다.
+첫째, 나의 죄와 비참함이 얼마나 큰가,
+둘째, 나의 모든 죄와 비참함으로부터 어떻게 구원을 받는가,
+셋째, 그러한 구원을 주신 하나님께 어떻게 감사를 드려야 하는가를 알아야 합니다.
+#heidelberg_catechism', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '제가 섬기고 있는 눔코리아 그로스 마케팅팀에서 경력직 콘텐츠 마케터를 모십니다.
+국내 천만 다이어터들의 마음에 공감해주고, 이들과 함께 콘텐츠로 놀아주고 이들의 언어로 눔의 핵심가치 차별점들을 설명해주실 콘텐츠 전문가를 찾고 있습니다.
+많은 관심과 지원/추천 부탁드립니다.', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '#Noom #입사1주년 #기념떡
+그동안 감사했습니다. 앞으로도 잘부탁드려요.', null, '2016-09-28 11:12:55');
+INSERT INTO sw_crawler.documents (username, content, category, created_date) VALUES ('정성영', '다이어트는 80식 20운동이라는 얘기가 있다. 책 제목으로 쓰일 정도로 이 업계에서는 일반화 된 표현이다.
+건강한 체력과 마인드를 위해 20운동이라는건 충분히 공감하고, 이해되지만 사실 체중 감량으로써 다이어트 입장에서는 20도 많다. 95%식습관 5%운동 정도라고 본다.
+다이어트와 운동의 연관성을 대중들에게 알리게된건 코카콜라, 나이키 들의 (사람들이 운동하면 돈을 버는) 브랜드들이 엄청난 양의 IMC 광고캠페인을 운영하기 시작하면서 인데, 심지어 코카콜라는 운동과 다이어트의 연관성을 설명하는 연구결과를 발표하는 연구자들에게 연구비를 후원해줬다.
+1시간 운동한걸 잘못된 식습관을 갖고 있는 사람들은 5분만에 도로묵으로 만들고, 우리 다이어터들은 이런 사람들을 ''건돼: 건강한돼 지''라고도 부른다. 문제는 습관이 고쳐지지 않으면 나이가 갈수록 대사량이 줄기때문에 체중과 (비만이 건강에 불러오는) 병들이 나타난다.
+아는만큼 보이는 다이어트
+#YouAreWhatYouEat #Noom #식습관코칭', 'entertain', '2016-09-28 11:12:55');
